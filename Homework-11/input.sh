6a09fe05e1e8e4f488d4ab2de4467295d098043b
BibigulKosherova <bibigulkosherova@gmail.com>
2025-02-28 13:19:53 +0000

    corrected input.sh

--- Homework-11/input.sh
+++ Homework-11/input.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-echo "Hi, my name is $1, and I'm $2 years old. Every morning, $1 likes to spend time $4.
-As a $2-year-old, I believe it's never too late to follow your dreams. My favorite programming language is $3.
-I code in $3 almost every day!
-One day, I hope my love for $3 will help me achieve my dream of $5.
-When I'm not busy working toward $5, you'll find me $4 to relax.
+
+read -p "What is your name? " name
+read -p "How old are you? " age
+read -p "What do you like to do in your free time? " hobby
+read -p "What is your dream? " dream
+read -p "What programming language do you like? " favorite_language
+echo "Hi, my name is $name, and I'm $age years old. Every morning, $name likes to spend time $hobby.
+As a $age-year-old, I believe it's never too late to follow your dreams. My favorite programming language is $favorite_language.
+I code in $favorite_language almost every day!
+One day, I hope my love for $favorite_language will help me achieve my dream of $dream.
+When I'm not busy working toward $dream, you'll find me $hobby to relax.
 That's a little about me, $1 — a $2-year-old who loves $3, enjoys $4, and dreams of $5!" 
