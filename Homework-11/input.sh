2ba9f1bfeb06b2c82d051b53b5b0f2798eeffd8d
BibigulKosherova <bibigulkosherova@gmail.com>
2025-02-28 04:02:22 +0000

    homework-11

---
+++ Homework-11/input.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Hi, my name is $1, and I'm $2 years old. Every morning, $1 likes to spend time $4.
+As a $2-year-old, I believe it's never too late to follow your dreams. My favorite programming language is $3.
+I code in $3 almost every day!
+One day, I hope my love for $3 will help me achieve my dream of $5.
+When I'm not busy working toward $5, you'll find me $4 to relax.
+That's a little about me, $1 — a $2-year-old who loves $3, enjoys $4, and dreams of $5!" 
